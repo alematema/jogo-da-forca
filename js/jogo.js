ce229d0b7257362da7446482e5f4f6e3f2e22130
Alexandre Siqueira <alematema@gmail.com>
2018-05-12 23:01:37 -0300

    add processaChute

--- js/jogo.js
+++ js/jogo.js
@@ -1,15 +1,34 @@
-var criaJogo = function () {
+var criaJogo = function (sprite) {
 
 	var palavraSecreta;
 	var etapa = 1;
 	var lacunas = [];
+	
+	function chuteErrado(chute) {
+		return palavraSecreta.indexOf(chute) == -1;
+	}
+
+	var processaChute = function (chute){
+		
+		if( chuteErrado(chute) ){
+			sprite.nextFrame();
+		}else{
+			setLacunas(chute);
+		}
+	}
 
 	var setPalavraSecreta = function (palavra) {
 		palavraSecreta = palavra;
-		setLacunas(palavra.length);
+		criaLacunas(palavra.length);
 		proximaEtapa();
 	};
 
+	var setLacunas = function (chute){
+		for( var i = 0; i < palavraSecreta.length; i++ ){
+			if( palavraSecreta.charAt(i) == chute) lacunas[i] = chute;
+		}
+	}
+	
 	var getLacunas = function () {
 		return lacunas;
 	};
@@ -18,18 +37,19 @@ var criaJogo = function () {
 		return etapa;
 	};
 
-	var setLacunas = function (quantidade) {
-		for (var i = 0; i < quantidade; i++) lacunas.push('');
+	var criaLacunas = function (length) {
+		lacunas = Array(length).fill('');
 	}
 
 	var proximaEtapa = function () {
 		etapa++;
 	}
-
+	//API
 	return {
 		setPalavraSecreta: setPalavraSecreta,
 		getLacunas: getLacunas,
-		getEtapa: getEtapa
+		getEtapa: getEtapa,
+		processaChute : processaChute
 	};
 
 }
