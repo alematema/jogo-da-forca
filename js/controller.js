1907520fe562ecc8d9b0ddfc1c60593000967d2c
Alexandre Siqueira <alematema@gmail.com>
2018-05-13 16:41:42 -0300

    implementacao etapa 2 do jogo

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,19 @@
 var criaController = function (jogo) {
-	
+
 	$entrada = $('.entrada');
 	$lacunas = $('.lacunas');
 
 	// consulta jogo.getLacunas() e exibe para o usuário cada lacuna 
 	var exibeLacunas = function () {
-		for( var i = 0; i < jogo.getLacunas().length; i++ ){
-				$lacunas.append($('<div>').addClass('lacuna'));
-		}
+		$lacunas.empty();
+		jogo.getLacunas().forEach(function (lacuna) {
+			$('<li>')
+				.addClass('lacuna')
+				.text(lacuna)
+				.appendTo($lacunas);
+		});
 	};
+
 
 	// muda o texto do placeHolder do campo de entrada    
 	var mudaPlaceHolder = function (texto) {
@@ -18,7 +23,15 @@ var criaController = function (jogo) {
 
 	// passa para jogo.setPalavraSecreta() o valor digitado pelo jogador e chama o a função `exibeLacunas()` e `mudaPlaceHolder()` definidas no controller. 
 	var guardaPalavraSecreta = function () {
-		jogo.setPalavraSecreta($entrada.val());
+		jogo.setPalavraSecreta($entrada.val().trim());
+	};
+
+	var leChute = function () {
+		jogo.processaChute($entrada.val().trim());
+	};
+
+	var limpaEntrada = function () {
+		$entrada.val('');
 	};
 
 	// faz a associação do evento keypress para capturar a entrada do usuário toda vez que ele teclar ENTER
@@ -26,13 +39,32 @@ var criaController = function (jogo) {
 		$entrada.keypress(function (event) {
 			if (event.which == 13) {
 				switch (jogo.getEtapa()) {
+
 					case 1:
 						guardaPalavraSecreta();
 						exibeLacunas();
 						mudaPlaceHolder('chute');
 						break;
+
 					case 2:
-						alert('etapa 2 - falta implementar');
+
+						if ( ! jogo.ganhouOuPerdeu() ) {
+							
+							leChute();
+							
+							exibeLacunas();
+
+							if (jogo.ganhou()) {
+								mudaPlaceHolder('GANHOU');
+								$entrada.attr('disabled', true);
+							} else if (jogo.perdeu()) {
+								mudaPlaceHolder('PERDEU');
+								$entrada.attr('disabled', true);
+							}
+							
+							limpaEntrada();
+						}
+
 						break;
 				}
 			}
